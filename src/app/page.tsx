5420ff651922f8cea1e7dcca134e99f33d32dc3f
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-09-22 13:24:52 -0400

    DateChange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function HomePage() {
                 Women's Physical Exercise (PE):
               </p>
               <p className="text-black">
-                When: <span className="font-semibold">(Sep 15 - Oct 31) 2025</span>
+                When: <span className="font-semibold">(Oct 5 - Oct 31) 2025</span>
               </p>
               <p className="text-black">
                 Where: <span className="font-semibold">Zoom</span>
